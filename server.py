c47f368d5ac26ffc6b3abc087b6d201491744ebf
Vargha90 <Varghalaszlo90@gmail.com>
2021-11-23 11:23:07 +0100

    Question headers added

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-
-import connection
+import data_manager
 
 app = Flask(__name__)
 
@@ -12,29 +11,30 @@ def main_page():
 
 @app.route("/list")
 def get_list():
-    forum_data = connection.import_questions
-    return render_template("list.html", forum_data=forum_data)
+    question_headers = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+    return render_template('list.html', question_headers=question_headers)
 
 
-@app.route("/question/<question_id>")
+# @app.route("/question/<question_id>")
 # @app.route("/question/<question_id>/new-answer")
 # @app.route("/question/<question_id>/delete")
 # @app.route("/question/<question_id>/edit")
 # @app.route("/question/<question_id>/vote_up and /question/<question_id>/vote_down")
-def get_question(question_id=None):
-    return render_template("index.html", question_id=question_id)
-
-
-@app.route("/add-question")
-def add_question():
-    return "Add_question"
-
-
-@app.route("/answer/<answer_id>/delete ")
-# @app.route("/answer/<answer_id>/vote_up and /answer/<answer_id>/vote_down ")
-def get_answer(answer_id=None):
-    return render_template("index.html", answer_id=answer_id)
+# def get_question(question_id=None):
+#     return render_template("index.html", question_id=question_id)
+#
+#
+# @app.route("/add-question")
+# def add_question():
+#     return "Add_question"
+#
+#
+# @app.route("/answer/<answer_id>/delete ")
+# # @app.route("/answer/<answer_id>/vote_up and /answer/<answer_id>/vote_down ")
+# def get_answer(answer_id=None):
+#     return render_template("index.html", answer_id=answer_id)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000,
+            debug=True)
